2d0a24f73c950f59c2c35855a9b05b0253c16335
Sokly Hour <hoursokly011@gmail.com>
2024-10-26 16:34:43 -0500

    add all files to assignment3

---
+++ Assignment3/service-worker.js
@@ -0,0 +1,51 @@
+// Cache name
+const CACHE_NAME = 'micro-tracker-cache-v1';
+
+// Files to cache
+const URLS_TO_CACHE = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/main.js',
+    './img/mylogo.png',
+    './img/nutritionsummarypicture.jpeg',
+    'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js'
+];
+
+// Install event
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(URLS_TO_CACHE);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return cached response if available, otherwise fetch from network
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
